b24468118cef936e011aea9bdde5a8217c0f5795
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-11-13 16:34:38 +0200

    доробив добавляння контактів

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,94 +1,63 @@
 import ContactsList from 'components/ContactsList/ContactsList';
 import FormBook from 'components/FormBook/FormBook';
 import FilterContacts from '../FilterContacts/FilterContacts';
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import useLocalStorage from 'Hooks/useLocalStorage';
 // імпорт стилів
 import { AppContainer, H2 } from './App.styled';
 
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage('name', []);
+  const [filter, setFilter] = useState('');
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  // добавдяємо контакт та номер телефону по сабміту та появляння помилки,якщо ім'я та номер вже є в контакті
-  addContact = ( name, number ) => {
-    const contact = {
+  const addContact = (name, number) => {
+    const newContact = {
       id: nanoid(),
       name,
       number,
     };
-
-    const findContact = this.state.contacts.find(contact =>
-      contact.name.toLowerCase().includes(name.toLowerCase())
-    );
-
-    findContact
-      ? alert(`${name} is already in contact`)
-      : this.setState(({ contacts }) => ({
-          contacts: [contact, ...contacts],
-        }));
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contact`);
+      return;
+    }
+    setContacts(contacts => [newContact, ...contacts]);
   };
+
   // Метод видалення контакту по ID //Працюємо лишe з map(), reduce(), filter()
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
   };
   // фільтр контактів
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
   };
-  getFindContacts = () => {
-    const { filter, contacts } = this.state;
+
+  const getFindContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  // добавлення контактів в локал сторедж
-  componentDidUpdate(prevProps, prevState) {
-    // перевірка поля на не рівність
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  // сейтить ізначальний state данних
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if(parsedContacts){
-      this.setState({contacts: parsedContacts});
-    }
-  }
-
-  render() {
-    const findContacts = this.getFindContacts();
-    return (
-      <>
-        <AppContainer>
-          <H2>Contacts</H2>
-          <FormBook onSubmit={this.addContact} />
-          <H2>Contacts list</H2>
-          <FilterContacts
-            filter={this.state.filter}
-            changeFilter={this.changeFilter}
-          />
-          <ContactsList
-            contacts={findContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </AppContainer>
-      </>
-    );
-  }
+  return (
+    <>
+      <AppContainer>
+        <H2>Contacts</H2>
+        <FormBook onSubmit={addContact} />
+        <H2>Contacts list</H2>
+        <FilterContacts filter={filter} changeFilter={changeFilter} />
+        <ContactsList
+          contacts={getFindContacts()}
+          onDeleteContact={deleteContact}
+        />
+      </AppContainer>
+    </>
+  );
 }
-export default App;
--- src/components/FormBook/FormBook.jsx
+++ src/components/FormBook/FormBook.jsx
@@ -31,6 +31,7 @@ export default function FormBook({ onSubmit }) {
 
     reset();
   };
+  
   const reset = () => {
     setName('');
     setNumber('');
